1fe88dac5ab2d844a7c270c964e54fefcd28bc4e
flopezcardozo <flopezcardozo@gmail.com>
2021-12-16 04:05:51 +0000

    no renderiza

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar";
+import Body from "./body";
+import Footer from "./footer";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar />
+			<Body />
+			<Footer />
 		</div>
 	);
 };
